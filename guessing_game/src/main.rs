46d6826d8b0399b9090a59a4d3c51aecb6e915d8
zukaishi <ishizuka215@gmail.com>
2021-10-06 08:43:55 +0900

    update

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,9 +3,8 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
+    println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number); 
-
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
@@ -17,9 +16,7 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        println!("You guessed: {}",guess);
-        
-        println!("Please input your guess.");
+        println!("You guessed: {}",guess);        
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
