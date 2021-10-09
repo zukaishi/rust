f1bcc959d9d13ffa4c2d601bdbc3e431d07b61c6
zukaishi <ishizuka215@gmail.com>
2021-10-10 07:19:18 +0900

    update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,4 +10,12 @@ fn main() {
 
     let guess: u32 = "42".parse().expect("Not a number!");   
     println!("{}",guess);
+
+    let t = true;
+    let f: bool = false;
+    println!("{},{}",t,f);
+
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of x is : {},y is: {}, z is :{}",x, y,z);
 }
