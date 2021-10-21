86c5c87cb601cc64029fe73c1747e85b4f20c2e5
zukaishi <ishizuka215@gmail.com>
2021-10-22 06:45:45 +0900

    update

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,3 +1,7 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("{}",guess);
 }
