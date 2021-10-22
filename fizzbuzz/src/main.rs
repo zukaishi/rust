978a8cc7a05cedcd869174c25449d3ef7348abfc
zukaishi <ishizuka215@gmail.com>
2021-10-23 06:57:05 +0900

    update

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    fizzbuzz1();
+    fizzbuzz2();
+}
+fn fizzbuzz1(){
+    println!("### fizzbuzz1 ###");
     for i in 1..16 {
         let mut s: String = "".to_string();
         if i % 3 == 0 {
@@ -8,5 +13,16 @@ fn main() {
             s.push_str("buzz");
         }
         println!("{}{}",i,s);
+    }
+}
+fn fizzbuzz2(){
+    println!("### fizzbuzz2 ###");
+    for i in 1..16 {
+        match i {
+            a if a % 15 == 0 => println!("FizzBuzz"),
+            a if a % 3 == 0 => println!("Fizz"),
+            a if a % 5 == 0 => println!("Buzz"),
+            a => println!("{}", a),
+        }
     }
 }
