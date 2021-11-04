6446ee0bde9e1b76f64afab4ec8e85aa78e6bee6
zukaishi <ishizuka215@gmail.com>
2021-11-05 05:58:17 +0900

    update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,12 +3,17 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
 }
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {}", x);
@@ -160,6 +165,11 @@ fn main() {
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
+    );
+    let sq = Rectangle::square(3);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        sq.area()
     );
 }
 
